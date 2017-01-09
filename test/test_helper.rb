ac68aea79388709868bbbfb3a2deb13904af3229
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-01-09 13:36:02 -0800

    Tests - updated for codeclimate-test-reporter > 1.0

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,8 @@
 require "bundler/setup"
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 require "simplecov"
 SimpleCov.start
 
 require "dpn/bagit"
 require "test/unit"
 require "test_case"
-
