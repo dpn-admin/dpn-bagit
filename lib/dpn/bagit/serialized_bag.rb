eda1248b5c2630a8eb7e971ca99efdc3db6e7d9f
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-01-09 13:28:07 -0800

    SerializedBag - refactor and fix bug for missing `file` variable

--- lib/dpn/bagit/serialized_bag.rb
+++ lib/dpn/bagit/serialized_bag.rb
@@ -1,62 +1,61 @@
 require "dpn/bagit/bag"
 
-
+##
 # A wrapper for a serialized Bag-It bag on disk.
 # Once created, will not change with changes made to the underlying filesystem
 # bag; in that case, a new object should be created.
+# @!attribute [r] location
+#   @return [String] The location, which can be relative or absolute.
 class DPN::Bagit::SerializedBag
 
+  attr_reader :location
+
   # Create a SerializedBag
-  # @param _location [String] Path to the file.
-  def initialize(_location)
-    if File.exists?(_location) == false
-      raise ArgumentError, "File does not exist!"
-    end
-
-    @location = _location
-    @cachedFixity = nil
+  # @param path [String] Path to the file.
+  def initialize(path)
+    raise ArgumentError, "File does not exist!" unless File.exist?(path)
+    @location = path
   end
 
-
-  # Returns the size of the serialized bag.
-  # @return [Fixnum] Apparent size of the bag in bytes.
-  def size()
-    return File.size(@location)
+  # Returns the file name for the serialized bag, without it's extension.
+  # @return [String] name
+  def name
+    @name ||= File.basename(location, File.extname(location))
   end
 
-
-  # Returns the local file location of the Bag.
-  # @return [String] The location, which can be relative or absolute.
-  def location()
-    return @location
+  # Returns the directory path to the serialized bag.
+  # @return [String] path
+  def path
+    @path ||= File.dirname(location)
   end
 
+  # Returns the size of the serialized bag (in bytes).
+  # @return [Fixnum] size
+  def size
+    File.size(location)
+  end
 
   # Returns the fixity of the serialized version of the bag.
   # @param algorithm [Symbol] The algorithm to use for calculation.
-  # @return [String] The fixity of the file.
+  # @return [String] fixity
   def fixity(algorithm)
-    if @cachedFixity == nil
+    @cachedFixity ||= begin
       case algorithm
       when :sha256
         digest = Digest::SHA256
       else
         raise ArgumentError, "Unknown algorithm."
       end
-
-      @cachedFixity = digest.file(@location).hexdigest
+      digest.file(location).hexdigest
     end
-    return @cachedFixity
   end
-
 
   # Unserialize the bag into the local filesystem.  This object
   # is unchanged.  Requires sufficient permissions and disk space.
-  # @return [Bag] A bag made from the unserialized object.
-  def unserialize!()
-    `/bin/tar -xf #{@location} -C #{File.dirname(@location)}`
-    raise RuntimeError, "cannot untar #{file}" unless $?.success?
-    name = File.basename(@location).to_s.sub(/\..*/,'')    # remove the file extension
-    return DPN::Bagit::Bag.new(File.join(File.dirname(@location), name))
+  # @return [DPN::Bagit::Bag] A bag made from the unserialized object.
+  def unserialize!
+    `/bin/tar -xf #{location} -C #{path} 2> /dev/null`
+    raise RuntimeError, "cannot untar #{location}" unless $?.success?
+    DPN::Bagit::Bag.new(File.join(path, name))
   end
 end
--- test/unit/serialized_bag_test.rb
+++ test/unit/serialized_bag_test.rb
@@ -2,59 +2,79 @@ require "test_helper"
 
 class SerializedBagTest < TestCase
 
-     # Called before every test method runs. Can be used
-     # to set up fixture information.
-     def setup
-          super()
-          @settings = DPN::Bagit::Settings.instance
-          @location = File.join(@settings[:root],'test', 'test_objects/serialized_bag_test/supplies/a7b18eb0-005f-11e3-8ebb-f23c91aec05e.tar')
-          @sandbox = File.join(@settings[:root], 'test', 'test_objects/serialized_bag_test/sandbox/')
-          if File.exists?(@sandbox) == false
-               FileUtils.mkdir_p(@sandbox)
-          end
-     end
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    super()
+    @settings = DPN::Bagit::Settings.instance
+    @location = File.join(@settings[:root],'test', 'test_objects/serialized_bag_test/supplies/a7b18eb0-005f-11e3-8ebb-f23c91aec05e.tar')
+    @sandbox = File.join(@settings[:root], 'test', 'test_objects/serialized_bag_test/sandbox/')
+    FileUtils.mkdir_p(@sandbox) unless File.exist?(@sandbox)
+  end
 
-     # Called after every test method runs. Can be used to tear
-     # down fixture information.
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
 
-     def teardown
-          FileUtils.rm_rf(File.join(@sandbox, '*'))
-     end
+  def teardown
+    FileUtils.rm_rf(File.join(@sandbox, '*'))
+  end
 
-     def test_init()
-          serializedBag = DPN::Bagit::SerializedBag.new(@location)
-          assert(serializedBag != nil, "init failed")
-     end
+  def bag
+    DPN::Bagit::SerializedBag.new(@location)
+  end
 
+  def test_init()
+    assert(bag != nil, "init failed")
+  end
 
-     def test_getLocation
-          bag = DPN::Bagit::SerializedBag.new(@location)
-          assert(bag.location() == @location, "location mismatch")
-     end
+  def test_getLocation
+    assert(bag.location == @location, "location mismatch")
+  end
 
+  def test_getName
+    name = File.basename(@location, File.extname(@location))
+    assert(bag.name == name, "name mismatch")
+  end
 
-     def test_getSize
-          bag = DPN::Bagit::SerializedBag.new(@location)
-          size  = bag.size()
-          assert(size < 9921061 * 1.1, "size greater than expected * 1.1")
-          assert(size > 9921061 * 0.9, "size less than expected * 0.9")
-     end
+  def test_getPath
+    path = File.dirname(@location)
+    assert(bag.path == path, "path mismatch")
+  end
 
+  def test_getSize
+    size = bag.size
+    assert(size < 9921061 * 1.1, "size greater than expected * 1.1")
+    assert(size > 9921061 * 0.9, "size less than expected * 0.9")
+  end
 
-     def test_getFixity
-          bag = DPN::Bagit::SerializedBag.new(@location)
-          fixity = '3971022d741a7f4f25775ef5aefe0f23a4cea18dc9f3563e832f7f08de3ac0a2'
-          assert_equal(fixity, bag.fixity(:sha256))
-     end
+  def test_getFixity
+    fixity = '3971022d741a7f4f25775ef5aefe0f23a4cea18dc9f3563e832f7f08de3ac0a2'
+    assert_equal(fixity, bag.fixity(:sha256))
+  end
 
+  def test_getFixityException
+    assert_raise ArgumentError do
+      bag.fixity(:sha1)
+    end
+  end
 
-     def test_unserialize
-          FileUtils.cp(@location, @sandbox)
-          serializedBag = DPN::Bagit::SerializedBag.new(File.join(@sandbox, File.basename(@location)))
-          bag = serializedBag.unserialize!()
-          assert(bag.valid?, "unserialized bag version isn't valid.")
-     end
+  def test_unserialize
+    FileUtils.cp(@location, @sandbox)
+    serializedBag = DPN::Bagit::SerializedBag.new(File.join(@sandbox, File.basename(@location)))
+    bag = serializedBag.unserialize!
+    assert(bag.valid?, "unserialized bag isn't valid.")
+  end
 
-
-
+  def test_unserializeException
+    assert_raise RuntimeError do
+      FileUtils.cp(@location, @sandbox)
+      tar_path = File.join(@sandbox, File.basename(@location))
+      assert(File.exist?(tar_path))
+      zip_path = tar_path + '.zip'
+      system("zip -q #{zip_path} #{tar_path}")
+      assert(File.exist?(zip_path))
+      serializedBag = DPN::Bagit::SerializedBag.new(zip_path)
+      serializedBag.unserialize!
+    end
+  end
 end
